4457115f545fec7444ecc149cc9ddc2e22abf676
fromSoundToDev <jeunejuste@gmail.com>
2024-12-12 00:51:24 +0000

    create dashboard and login routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import React from 'react'
+import LoginForm from './component/LoginForm'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/login'
+
 export default function App() {
   return (
-    <h1 className="text-xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginForm/>} ></Route>
+          <Route path="/" element={<Login/>} ></Route>
+          <Route path="/dashboard" element={<Dashboard/>} ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+
   )
 }
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='bg-gradient-to-b from-green-500 from-50% to-white to-50% h-screen'>
+
+    </div>
+  )
+}
